94c03cb6a00dae72ad1b9b3cf47a4718e7093ac9
12wang3 <583268345@qq.com>
2016-05-03 21:45:33 +0800

    Create the project.

---
+++ CloudDuan/userUnit/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from userUnit.models import CdUser
+# Register your models here.
+
+admin.site.register(CdUser)
---
+++ CloudDuan/userUnit/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from  django.contrib.auth.models import User
+# Create your models here.
+
+class CdUser(models.Model):
+    user = models.OneToOneField(User)
+    def __unicode__(self):
+        return self.user.username

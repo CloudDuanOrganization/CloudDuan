8f2f9af0f46b6750b4f5c9305bd0d8cc43156691
12wang3 <583268345@qq.com>
2016-05-06 17:39:03 +0800

    Add user login module.

---
+++ CloudDuan/userUnit/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^userLogin$', views.userLogin, name='userLogin'),
+    # url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
+    # url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
+    # url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
+]

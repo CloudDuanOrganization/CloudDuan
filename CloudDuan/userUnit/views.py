68f515dbfece8c246d75774d96528503101a3240
12wang3 <583268345@qq.com>
2016-05-07 19:55:59 +0800

    Finish the user login module.

--- CloudDuan/userUnit/views.py
+++ CloudDuan/userUnit/views.py
@@ -2,20 +2,25 @@ from django.shortcuts import render,render_to_response,HttpResponse
 from django.contrib.auth import authenticate, login
 
 def userLogin(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    user = authenticate(username=username, password=password)
-    if user is not None:
-        if user.is_active:
-            login(request, user)
-            # Redirect to a success page.
-            return HttpResponse('Login Successfully!')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                # Redirect to a success page.
+                return render_to_response('signin.html',{'login_err':'Login Successfully!'})
+            else:
+                # Return a 'disabled account' error message
+                return render_to_response('signin.html',{'login_err':'Disabled Account!'})
+
         else:
-            # Return a 'disabled account' error message
-            return HttpResponse('Disabled Account!')
+            # Return an 'invalid login' error message.
+            return render_to_response('signin.html',{'login_err':'Invalid Login!'})
+    return render_to_response('signin.html',{})
 
-    else:
-        # Return an 'invalid login' error message.
-        return HttpResponse('Invalid Login!')
-
-
+def userRegister(request):
+    if request.method == 'POST':
+        pass
+    return HttpResponse('Register Page')

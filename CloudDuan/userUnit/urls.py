f38b27c7bbb5d7ac11f788af4e5982fcda8fda9e
12wang3 <583268345@qq.com>
2016-05-07 21:09:03 +0800

    Add user logout and user register view.

--- CloudDuan/userUnit/urls.py
+++ CloudDuan/userUnit/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^userLogin/$', views.userLogin, name='userLogin'),
+    url(r'^userRegister/$', views.userRegister, name='userRegister'),
     # url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
     # url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
     # url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),

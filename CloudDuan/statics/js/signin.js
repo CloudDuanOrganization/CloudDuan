0b67329b3f3883a30bd8157c1e37e27e6a099c92
SunskyXH <y445360776@qq.com>
2016-05-08 20:25:41 +0800

    Add register page.

--- CloudDuan/statics/js/signin.js
+++ CloudDuan/statics/js/signin.js
@@ -4,3 +4,26 @@ $('#formbackground').width($(window).width());
 });
 
 
+function checkRegisterForm() {
+            var user = document.RegisterForm.username.value;
+            var pwd1 = document.RegisterForm.password.value;
+            var pwd2 = document.RegisterForm.rePassword.value;
+            if(user.length===0) {
+                alert("Username should not be null!");
+                document.RegisterForm.username.focus();
+                return false;
+            }
+            if (pwd1!==pwd2) {
+                alert("password not same");
+                return false;
+            }
+            if (pwd1.length<6 ) {
+                alert("password to short")
+                return false;
+            }
+            if (pwd1.length>40 ) {
+                alert("password to long")
+                return false;
+            }
+            
+        }

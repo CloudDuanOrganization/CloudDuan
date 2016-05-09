515b409f22b690e8d7dce4b850ec475c6a53ad05
12wang3 <583268345@qq.com>
2016-05-09 17:55:17 +0800

    Change HttpResponse to JsonResponse.

--- CloudDuan/userUnit/views.py
+++ CloudDuan/userUnit/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, render_to_response, HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from django.http import JsonResponse
 from userUnit.models import CdUser
 
 def userLogin(request):
@@ -12,11 +13,11 @@ def userLogin(request):
             if user.is_active:
                 login(request, user)
                 #TODO: Redirect to a success page.
-                return render_to_response('signin.html',{'login_err':'登陆成功!', 'login_flag':0})
+                return JsonResponse({'login_err':u'登陆成功!', 'login_flag':0})
             else:
-                return render_to_response('signin.html',{'login_err':'账号未激活!', 'login_flag':1})
+                return JsonResponse({'login_err':u'账号未激活!', 'login_flag':1})
         else:
-            return render_to_response('signin.html',{'login_err':'登陆失败!', 'login_flag':2})
+            return JsonResponse({'login_err':u'登陆失败!', 'login_flag':2})
     return render_to_response('signin.html',{})
 
 def userLogout(request):
@@ -29,18 +30,18 @@ def userRegister(request):
         email = request.POST['email']
         password = request.POST['password']
         rePassword = request.POST['rePassword']
-        if (not username) or len(username)<6 or len(username)>40:
-            return render_to_response('signin.html',{'register_err':'用户名非法!','register_flag':0})
+        if (not username) or len(username)<2 or len(username)>40:
+            return JsonResponse({'register_err':u'用户名非法!','register_flag':0})
         if (not password) or len(password)<6 or len(password)>40:
-            return render_to_response('signin.html',{'register_err':'密码非法!','register_flag':0})
+            return JsonResponse({'register_err':u'密码非法!','register_flag':0})
         if password != rePassword:
-            return render_to_response('signin.html',{'register_err':'两次密码不一致!','register_flag':0})
+            return JsonResponse({'register_err':u'两次密码不一致!','register_flag':0})
         #TODO: Check email format
         try:
             newUser = CdUser()
             newUser.user = User.objects.create_user(username, email, password)
             newUser.save()
-            return render_to_response('signin.html',{'register_err':'用户注册成功!','register_flag':1})
+            return JsonResponse({'register_err':u'用户注册成功!','register_flag':1})
         except:
-            return render_to_response('signin.html',{'register_err':'用户已存在!','register_flag':0})
+            return JsonResponse({'register_err':u'用户已存在!','register_flag':0})
     return HttpResponse('Register Page')

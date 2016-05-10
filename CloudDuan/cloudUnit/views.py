6702a554984c839e2a123a565e33a20bc53f69bc
SunskyXH <y445360776@qq.com>
2016-05-10 22:42:31 +0800

    Add Index page(navbar).

--- CloudDuan/cloudUnit/views.py
+++ CloudDuan/cloudUnit/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,render_to_response
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Index Page.')
+    return render_to_response('index.html')

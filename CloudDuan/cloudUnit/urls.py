28fc9199ea56c9d87287826499cf7c3345ad725f
12wang3 <583268345@qq.com>
2016-05-12 22:29:27 +0800

    Add duanPublish view.

--- CloudDuan/cloudUnit/urls.py
+++ CloudDuan/cloudUnit/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    # url(r'^userLogin/$', views.index, name='userLogin'),
+    url(r'^duanPublish/$', views.duanPublish, name='duanPublish'),
     # url(r'^userRegister/$', views.userRegister, name='userRegister'),
     # url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
     # url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
--- CloudDuan/cloudUnit/views.py
+++ CloudDuan/cloudUnit/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render,HttpResponse,render_to_response
 
 def index(request):
     return render_to_response('index.html')
+
+def duanPublish(request):
+    if request.method == 'POST':
+        print(request.body)
+        print(str(request.body))
+    return HttpResponse('duanPublish')

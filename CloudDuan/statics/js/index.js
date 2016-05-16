9a2709df1cd1e00e1f0a00970f811c0bffb8f8f7
SunskyXH <y445360776@qq.com>
2016-05-16 23:18:12 +0800

    上传封面（修改2）

--- CloudDuan/statics/js/index.js
+++ CloudDuan/statics/js/index.js
@@ -1,4 +1,3 @@
-
 $(function() {
     $(window).scroll(function() {
         if ($(window).scrollTop() >= 100) { //向下滚动像素大于这个值时，即出现小火箭~
@@ -10,17 +9,36 @@ $(function() {
     $('.actGotop').click(function() { $('html,body').animate({ scrollTop: '0px' }, 800); }); //火箭动画停留时间，越小消失的越快~
 });
 
+var coverUrl;
+//upload image
+function readFile(obj) {
+    var file = obj.files[0];
+    //判断类型是不是图片  
+    if (!/image\/\w+/.test(file.type)) {
+        alert("请确保文件为图像类型");
+        return false;
+    }
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function(e) {
+        coverUrl = this.result; //base64
+        // console.log(coverUrl);
 
+    }
+}
 
 
 $(document).ready(function() {
+
     //summernote init
     $('#summernote').summernote({
-        height: 300, 
-        minHeight: null, 
-        maxHeight: null, 
+        height: 300,
+        minHeight: null,
+        maxHeight: null,
         focus: true
     });
+
+
 
     //publish duan ajax
     $("#publishBtn").click(function() {
@@ -30,7 +48,7 @@ $(document).ready(function() {
             data: {
                 title: $("#inputTitle").val(),
                 content: $('#summernote').summernote('code'),
-                cover: $('#inputCover').val(),
+                cover: coverUrl,
             },
             dataType: 'json',
             success: function() {
@@ -41,7 +59,6 @@ $(document).ready(function() {
             },
         });
     });
-    //upload image
-    
+
 
 });

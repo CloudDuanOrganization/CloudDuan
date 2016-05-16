3ad4a5a056bca2cacafe2a0821044047577f6e1a
SunskyXH <y445360776@qq.com>
2016-05-16 18:08:22 +0800

    发布段子增加添加封面功能（未测试）。

--- CloudDuan/statics/js/index.js
+++ CloudDuan/statics/js/index.js
@@ -1,3 +1,4 @@
+
 $(function() {
     $(window).scroll(function() {
         if ($(window).scrollTop() >= 100) { //向下滚动像素大于这个值时，即出现小火箭~
@@ -10,59 +11,37 @@ $(function() {
 });
 
 
-// function sendFile(file,editor,$editable){
-//         var formData = new FormData();
-//         formData.append("file", file[0]);
-            
-//         $.ajax({
-//             data: formData,  
-//             type: "POST",  
-//             url: "/Product/UploadProductDescriptionImage",  
-//             cache: false,  
-//             contentType: false,  
-//             processData: false,  
-//             success: function(imageUrl) {  
-//                 editor.insertImage($editable, imageUrl);  
-//                 //$('.summernote').summernote('editor.insertImage',imageUrl);  
-//             },  
-//             error: function() {  
-                  
-//             }  
-//         })
-//     }
 
-
-// $('#publish').modal(options)
 
 $(document).ready(function() {
+    //summernote init
     $('#summernote').summernote({
-        height: 300, // set editor height
-        minHeight: null, // set minimum height of editor
-        maxHeight: null, // set maximum height of editor
-        focus: true // set focus to editable area after initializing summernote
-        // onImageUpload: function(files, editor, $editable) {
-        //         sendFile(files,editor,$editable);
-        //     },
+        height: 300, 
+        minHeight: null, 
+        maxHeight: null, 
+        focus: true
     });
 
-
+    //publish duan ajax
     $("#publishBtn").click(function() {
-
         $.ajax({
             url: '/cloudUnit/duanPublish/',
             type: 'POST',
-            dataType: 'json',
             data: {
                 title: $("#inputTitle").val(),
                 content: $('#summernote').summernote('code'),
+                cover: $('#inputCover').val(),
             },
-            success: function(data) {
+            dataType: 'json',
+            success: function() {
                 alert("发布成功!");
             },
             error: function(jqXHR) {
                 alert("发生错误：" + jqXHR.status);
             },
         });
-
     });
+    //upload image
+    
+
 });
--- CloudDuan/statics/js/signin.js
+++ CloudDuan/statics/js/signin.js
@@ -31,18 +31,18 @@ $(function() {
 
 
 $(document).ready(function() {
-//如果url中存在锚号,则显示锚号对应的tab
+    //如果url中存在锚号,则显示锚号对应的tab
     if (location.hash) {
         $('a[href=' + location.hash + ']').tab('show');
     }
-    
- //获取点击的标签的 href   
+
+    //获取点击的标签的 href   
     $(document.body).on("click", "a[data-toggle]", function(event) {
 
         location.hash = this.getAttribute("href");
 
     });
-    
+
 
 
 
@@ -58,95 +58,94 @@ $(window).on('popstate', function() {
 
 });
 // 处理注册表单
-    $("#registerBtn").click(function(){
-        $.ajax({
-            url: '/userUnit/userRegister/',
-            type: 'POST',
-            data:{
-                username: $("#inputUserName2").val(),
-                email: $("#inputEmail").val(),
-                password: $("#inputPassword2").val(),
-                rePassword: $("#inputRePassword").val(),
-            },
-            dataType: 'json',    
+$("#registerBtn").click(function() {
+    $.ajax({
+        url: '/userUnit/userRegister/',
+        type: 'POST',
+        data: {
+            username: $("#inputUserName2").val(),
+            email: $("#inputEmail").val(),
+            password: $("#inputPassword2").val(),
+            rePassword: $("#inputRePassword").val(),
+        },
+        dataType: 'json',
         success: function(data) {
-                if (data.register_flag == 1) {
-                    $("#registerResult-success").html(data.register_err);
-                    $("#div_registerResult-success").show();
-                    $("#div_registerResult-error").hide(); 
-                    setTimeout("location.href='/userUnit/userLogin/';", 1000);   
-                } else if(data.register_flag == 0) {
-                    $("#registerResult-error").html(data.register_err);
-                    $("#p_registerResult-error").html("请检查自己的输入。")
-                    
-                    $("#div_registerResult-success").hide();
-                    $("#div_registerResult-error").show();     
-                }  
-            },
-        error: function(jqXHR,data){     
-               alert("发生错误：" + jqXHR.status);
-            },  
-        });
-    });
-// 处理登录表单
-    $("#loginBtn").click(function(){
-        $.ajax({
-            url: '/userUnit/userLogin/',
-            type: 'POST',
-            dataType: 'json',
-            data:{
-                username:$("#inputUserName").val(),
-                password:$("#inputPassword").val(),
-            },
-        success: function(data) {
-                var url = window.location.search;
-                if(url){
-                    nextUrl = url.substring(url.lastIndexOf('=')+1, url.length);
-                }else {
-                    nextUrl = '/';
-                }
-                
-                if (data.login_flag == 0) {
-                    $("#loginResult-success").html(data.login_err);
-                    $("#div_loginResult-success").show();
-                    $("#div_loginResult-error").hide();
-                    setTimeout("top.location.href='"+nextUrl+"';", 1000);  
-                    // console.log(data.login_flag)
-                    // console.log(data.login_err)
-                } else if(data.login_flag == 2) {
-                    $("#loginResult-error").html(data.login_err);
-                    $("#p_loginResult-error").html("请检查自己的帐号密码。")
-                    $("#div_loginResult-error").show();
-                    $("#pbtn_loginResult-error").show();
-                    $("#div_loginResult-success").hide();
-                }else{
-                    $("#loginResult-error").html(data.login_err);
-                    $("#p_loginResult-error").html("请前去验证邮箱。")
-                    $("#div_loginResult-error").show();
-                    $("#div_loginResult-success").hide();
-                    $("#pbtn_loginResult-error").hide();
-                }
-                
-            },
-        error: function(jqXHR){     
-               alert("发生错误：" + jqXHR.status);  
-            },  
-        });
+            if (data.register_flag == 1) {
+                $("#registerResult-success").html(data.register_err);
+                $("#div_registerResult-success").show();
+                $("#div_registerResult-error").hide();
+                setTimeout("location.href='/userUnit/userLogin/';", 1000);
+            } else if (data.register_flag == 0) {
+                $("#registerResult-error").html(data.register_err);
+                $("#p_registerResult-error").html("请检查自己的输入。")
 
+                $("#div_registerResult-success").hide();
+                $("#div_registerResult-error").show();
+            }
+        },
+        error: function(jqXHR, data) {
+            alert("发生错误：" + jqXHR.status);
+        },
     });
+});
+// 处理登录表单
+$("#loginBtn").click(function() {
+    $.ajax({
+        url: '/userUnit/userLogin/',
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            username: $("#inputUserName").val(),
+            password: $("#inputPassword").val(),
+        },
+        success: function(data) {
+            var url = window.location.search;
+            if (url) {
+                nextUrl = url.substring(url.lastIndexOf('=') + 1, url.length);
+            } else {
+                nextUrl = '/';
+            }
+
+            if (data.login_flag == 0) {
+                $("#loginResult-success").html(data.login_err);
+                $("#div_loginResult-success").show();
+                $("#div_loginResult-error").hide();
+                setTimeout("top.location.href='" + nextUrl + "';", 1000);
+                // console.log(data.login_flag)
+                // console.log(data.login_err)
+            } else if (data.login_flag == 2) {
+                $("#loginResult-error").html(data.login_err);
+                $("#p_loginResult-error").html("请检查自己的帐号密码。")
+                $("#div_loginResult-error").show();
+                $("#pbtn_loginResult-error").show();
+                $("#div_loginResult-success").hide();
+            } else {
+                $("#loginResult-error").html(data.login_err);
+                $("#p_loginResult-error").html("请前去验证邮箱。")
+                $("#div_loginResult-error").show();
+                $("#div_loginResult-success").hide();
+                $("#pbtn_loginResult-error").hide();
+            }
+
+        },
+        error: function(jqXHR) {
+            alert("发生错误：" + jqXHR.status);
+        },
+    });
+
+});
 /** 
-*单击警告框的“x”来隐藏警告框
-**/
-    $("#close_register_error").click(function(){
-        $("#div_registerResult-error").hide();
-    });
-    $("#close_login_error").click(function(){
-        $("#div_loginResult-error").hide();
-    });
-    $("#close_register_success").click(function(){
-        $("#div_registerResult-success").hide();
-    });
-    $("#close_login_success").click(function(){
-        $("#div_loginResult-success").hide();
-    });
-    
+ *单击警告框的“x”来隐藏警告框
+ **/
+$("#close_register_error").click(function() {
+    $("#div_registerResult-error").hide();
+});
+$("#close_login_error").click(function() {
+    $("#div_loginResult-error").hide();
+});
+$("#close_register_success").click(function() {
+    $("#div_registerResult-success").hide();
+});
+$("#close_login_success").click(function() {
+    $("#div_loginResult-success").hide();
+});

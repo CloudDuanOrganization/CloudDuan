cd2a2da3d91eae72053627c9fe0e7f009aeae9da
SunskyXH <y445360776@qq.com>
2016-05-26 18:20:48 +0800

    修改list.html的显示方式

--- CloudDuan/statics/js/list.js
+++ CloudDuan/statics/js/list.js
@@ -1,22 +1,22 @@
-$(function(){
+$(document).ready(function(){
 	var url = document.location.toString();
     var arrUrl = url.split("//");
     var subUrl = arrUrl[1].split("/");
     var listType = subUrl[2];
-    var duanID = $("#duanID").val();
+
 
 
     if (listType === 'hotList'){
-    	$("#listTitle").html("<img src=\"/static/images/icon－热门推荐.png\">&nbsp;热门推荐</a>")
-    	$("#listLink").attr('href', '/cloudUnit/hotView/'+duanID+'/');
+    	$("#listTitle").html("<img src=\"/static/images/icon－热门推荐.png\">&nbsp;热门推荐</a>");
+
     }
     if (listType === 'newestList'){
     	$("#listTitle").html("<img src=\"/static/images/icon－新鲜段子.png\"> &nbsp;新鲜段子</a>")
-    	$("#listLink").attr('href', '/cloudUnit/newestView/'+duanID+'/');
+
     }
     if (listType === 'rankList'){
     	$("#listTitle").html("<img src=\"/static/images/icon－排行榜.png\"> &nbsp;排行榜</a>")
-    	$("#listLink").attr('href', '/cloudUnit/rankView/'+duanID+'/');
+
     }
 });
 
